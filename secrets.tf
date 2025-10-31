054a2b6ba62a0e2faba219c8d8886f8df1b6d771
Teshaylen Pillay <tesh@ikhokha.com>
2025-10-31 14:37:55 +0200

    fixed issue with secrets manager

--- secrets.tf
+++ secrets.tf
@@ -5,7 +5,7 @@ resource "aws_secretsmanager_secret" "database_credentials_postgres_final" {
 resource "aws_secretsmanager_secret_version" "database_credentials_version" {
   secret_id = aws_secretsmanager_secret.database_credentials_postgres_final.id
   secret_string = jsonencode({
-    username = "ec2user"
-    password = "StrongPassword123!"
+    username = "ec2user"            #replace with variable after testing
+    password = "StrongPassword123!" #replace with variable after testing
   })
 }
